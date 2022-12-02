01f6fe8c18cca7036b3c21289ba50e88bdaf0939
Antonino Musmeci <nino.musmeci@gmail.com>
2022-12-02 09:17:53 +0100

    day2 star1

---
+++ day2/star1.py
@@ -0,0 +1,14 @@
+max_calories = 0
+total = 0
+with open('day2/input.txt') as f:
+    for line in f:
+        line = line.strip('\n').split()
+        opponent_move =  ord(line[0]) - ord("A")      
+        my_move = ord(line[1]) - ord("X") 
+        total += my_move + 1
+        if (opponent_move + 1) %3 == my_move: 
+            total += 6
+        if opponent_move == my_move: 
+            total += 3
+    print(total)
+       
