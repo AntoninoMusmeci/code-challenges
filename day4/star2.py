ab3e6667f3e5c06b8ec8cf4b7c0377e342ccd387
Antonino Musmeci <nino.musmeci@gmail.com>
2022-12-04 11:22:02 +0100

    day4

---
+++ day4/star2.py
@@ -0,0 +1,11 @@
+result = 0
+with open('day4/input.txt') as f:
+    for line in f:
+        line = line.strip('\n')
+        assignments = line.split(',')
+        a1 = assignments[0].split('-')
+        a2 = assignments[1].split('-')
+        if int(a2[0]) <= int(a1[0]) <= int(a2[1]) or int(a2[0]) <= int(a1[1]) <= int(a2[1]) or \
+            int(a1[0]) <= int(a2[0]) <= int(a1[1]) or int(a1[0]) <= int(a2[1]) <= int(a1[1]):
+            result+=1
+    print(result)
